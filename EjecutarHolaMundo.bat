f70c6dda62f1c2f6efaad00e911a4c231d357d2e
Christian Jodra <c.jodra14@gmail.com>
2021-09-15 19:19:57 +0200

    Repaso HolaMundo

---
+++ EjecutarHolaMundo.bat
@@ -0,0 +1,6 @@
+cls
+set JAVAPATH=%JAVAPATH%;C:\Program Files\Java\jdk1.8.0_261\bin
+set PATH=%PATH%;%JAVAPATH%
+set CLASSPATH=%CLASSPATH%;C:\PracticasJava
+JAVAC prueba\HolaMundo.java 
+JAVA prueba.HolaMundo
